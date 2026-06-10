09ac62f0ee0102b6338a8a3003911c9f6d798cbf
Philippe6ak <philippe.siaka@gmail.com>
2026-06-10 08:23:28 +0000

    implement dock and animations

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from "#components/Navbar";
 import Welcome from "#components/Welcome";
+import Dock from "#components/Dock";
 
 //to clean up the imports in App.jsx, we can export all components from this file and import them in App.jsx
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock };
