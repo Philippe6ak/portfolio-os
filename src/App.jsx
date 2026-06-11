d632f68046dfabee2e8caa8f1579cd469dcf595e
Philippe6ak <philippe.siaka@gmail.com>
2026-06-11 14:51:42 +0000

    Window logic and terminal implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { Draggable } from "gsap/Draggable";
+import gsap from "gsap";
+
 import { Navbar, Welcome, Dock } from "#components";
+import { Terminal } from "#windows";
+
+gsap.registerPlugin(Draggable);
 
 function App() {
   return (
@@ -6,6 +12,8 @@ function App() {
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   );
 }
